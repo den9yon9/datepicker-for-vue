69b498c4cd058be74b261fd3bd54baed98564b83
den9yon9 <den9yon9@gmail.com>
2018-04-27 18:55:55 +0800

    add calendar panel

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <datepicker :range="range" :hiderestdate="hiderestdate" :disable="disable"/>
+  </div>
+</template>
+<script>
+import Datepicker from './components/Datepicker'
+
+export default {
+  name: 'App',
+  components: {
+    Datepicker
+  },
+  data() {
+    return {
+      range: {
+        start_notice: '入住',
+        end_notice: '离开',
+      },
+      hiderestdate: false,
+      disable: {
+        from: '2018-04-30',
+        to: '2018-04-12'
+      }
+    }
+  }
+}
+
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ src/components/Datepicker.vue
@@ -0,0 +1,355 @@
+<style scoped>
+.datepicker {
+  font-size: 18px;
+  width: 280px;
+  box-shadow: 0 0 4px #00000055;
+}
+
+.year-month {
+  display: flex;
+  justify-content: center;
+  flex-shrink: 0;
+  color: #666;
+  height: 40px;
+  align-items: center;
+}
+
+.next-month {
+  transform: rotate(90deg);
+  margin-left: 20px;
+  cursor: pointer;
+}
+
+.pre-month {
+  transform: rotate(-90deg);
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.year-month img {
+  width: 20px;
+}
+
+
+.days {
+  box-sizing: border-box;
+  border-bottom: solid 1px #f1f1f1;
+  display: flex;
+}
+
+.day {
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #666;
+}
+
+.dates {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.date {
+  width: 40px;
+  height: 40px;
+  color: #666;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  cursor: pointer;
+  transition: all 0.3s;
+  box-sizing: border-box;
+}
+
+
+
+/*隐藏属于上月与下月的格子*/
+
+.hide {
+  pointer-events: none;
+  opacity: 0
+}
+
+.pre,
+.next {
+  color: #ccc;
+}
+
+
+.disable {
+  background-color: #f8f8f8;
+  color: #ccc;
+  cursor: not-allowed;
+}
+
+.belong {
+  color: #585fee;
+  background-color: #f0f6ff;
+  transition: all 0.3s;
+}
+
+.notice {
+  font-size: 12px;
+}
+
+.belong.pre,
+.belong.next {
+  color: #deefff;
+}
+
+.active.pre,
+.active.next {
+  color: #fff;
+}
+
+.active {
+  background-color: #585fee;
+  color: #fff;
+  border-radius: 0.1rem;
+  transition: all 0.3s;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.disable.active,
+.disable.belong {
+  color: #aaa;
+}
+
+</style>
+<template>
+  <div class="datepicker">
+    <div class="year-month">
+      <a class="pre-month" @click="getPreMonth"><img src="../assets/arrow.png"></a>
+      <div class="year">{{year}}年</div>
+      <div class="month">{{month}}月</div>
+      <a class="next-month" @click="getNextMonth"><img src="../assets/arrow.png"></a>
+    </div>
+    <div class="days-dates">
+      <div class="days">
+        <div class="day" v-for="day of days">{{day}}</div>
+      </div>
+      <div class="dates">
+        <div class="date" v-for="date of dates" :class="getDateClass(date)" @click="getDate(date)">
+          <span>{{date.date}}</span>
+          <div class="notice" v-if="format(date.fullDate)==format(startdate)&&range">{{range.start_notice||'开始'}}</div>
+          <div class="notice" v-if="format(date.fullDate)==format(enddate)&&range">{{range.end_notice||'结束'}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { timeFormat } from '../utils.js'
+export default {
+  props: ['hiderestdate', 'range', 'disable', 'value'],
+  data() {
+    return {
+      days: ['日', '一', '二', '三', '四', '五', '六'],
+      months: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+      year: 0,
+      month: 0,
+      date: 0,
+      startdate: new Date(0),
+      enddate: new Date(0),
+      cnt: 0
+    }
+  },
+
+  mounted() {
+    if (this.range) {
+      this.startdate = this.range.startdate && new Date(this.range.startdate) || new Date()
+      this.enddate = this.range.enddate && new Date(this.range.enddate) || new Date(new Date().getTime() + 24 * 60 * 60 * 1000)
+    } else {
+      if (this.value) {
+        this.startdate = new Date(this.value)
+      }
+    }
+
+    this.year = this.startdate.getFullYear()
+    this.month = this.startdate.getMonth() + 1
+    this.date = this.startdate.getDate()
+  },
+
+  methods: {
+    format(date) {
+      return timeFormat(date)
+    },
+    // 判断是否是闰年
+    isLeapYear: year => year % 4 == 0 || year % 100 == 0 && year % 400 == 0,
+
+    // 获取当月天数
+    getMonthLen(year, month) {
+      return [2, 4, 6, 9, 11].indexOf(month) == -1 ? 31 : month == 2 ? this.isLeapYear(year) ? 29 : 28 : 30
+    },
+
+    // 当月从周几开始
+    getStartDay: (year, month) => new Date([year, month, 1].join('-')).getDay(),
+
+    // 获取方格中所有日子
+    getDates(year, month) {
+      // 获取本月日子
+      let len = this.getMonthLen(year, month)
+      let dates = Array.from(new Array(len), (v, i) => ({ date: i + 1, month: 'current' }))
+
+      // 这个月从周几开始
+      let startday = this.getStartDay(year, month)
+      // 上个月天数
+      let preMonthLen = this.getMonthLen(year, month - 1 || 12)
+
+      //  获取方格中上个月的日子置于方格头部
+      while (startday--) {
+        dates.unshift({ date: preMonthLen--, month: 'pre' })
+      }
+
+      // 获取方格中下个月的日子置于方格尾部
+      let restDates = Array.from(new Array(42 - dates.length), (v, i) => ({ date: i + 1, month: 'next' }))
+      dates = [...dates, ...restDates]
+      this.genDateInfo(dates)
+      // 返回方格中所有日子
+      return dates
+    },
+
+    // 生成每个格子里的完整日期和禁用信息
+    genDateInfo(dates) {
+      dates.forEach(n => {
+        let year = this.year
+        let month = this.month
+        if (n.month == 'pre') {
+          month = this.month - 1 || 12
+          if (month == 12) {
+            year = this.year - 1
+          }
+        } else if (n.month == 'next') {
+          month = this.month == 12 ? 1 : this.month + 1
+          if (month == 1) {
+            year = this.year + 1
+          }
+        }
+        n.fullDate = new Date([year, month, n.date].join('-'))
+
+        // 生成禁用信息，判断日子是否被禁用（选中）
+        if (this.disable) {
+          n.disable = n.fullDate < new Date(this.disable.to) || n.fullDate > new Date(this.disable.from)
+        } else {
+          n.disable = false
+        }
+      })
+    },
+
+    // 点击单个格子设置日期
+    getDate(date) {
+      if (date.disable) return;
+
+      // 离店日期与入住日期不能在同一天
+      if (timeFormat(date.fullDate) == timeFormat(this.startdate)) return;
+      this.date = date.date
+      if (date.month == 'pre') {
+        if (this.month == 1) {
+          this.month = 12
+          this.year--
+        } else {
+          this.month--
+        }
+      } else if (date.month == 'next') {
+        if (this.month == 12) {
+          this.month = 1
+          this.year++
+        } else {
+          this.month++
+        }
+      }
+      this.cnt++;
+      this.setDate()
+    },
+    // 单选双选模式逻辑
+    setDate() {
+      if (!this.range) {
+        this.startdate = new Date([this.year, this.month, this.date].join('-'))
+        this.$emit('datechange', this.startdate)
+      } else {
+        if (!(this.cnt % 2)) {
+          // 用户选择两个时间点，根据两个时间点的早晚决定那一天是开始哪一天是结束
+          let temp = new Date([this.year, this.month, this.date].join('-'))
+          if (temp > this.startdate) {
+            this.enddate = temp
+            this.$emit('enddate', this.enddate)
+          } else {
+            this.enddate = this.startdate
+            this.startdate = temp
+            this.$emit('startdate', this.startdate)
+            this.$emit('enddate', this.enddate)
+          }
+        } else {
+          this.enddate = new Date(0)
+          this.$emit('enddate', '')
+          this.startdate = new Date([this.year, this.month, this.date].join('-'))
+          this.$emit('startdate', this.startdate)
+        }
+      }
+    },
+
+    // 获取日期格子的样式
+    getDateClass(date) {
+      return [
+        date.month,
+        { active: this.hasActiveClass(date) },
+        { hide: this.hasHideClass(date) },
+        { disable: date.disable },
+        { belong: this.hasBelongClass(date) }
+      ]
+    },
+
+
+    // 判断格子中的日子是否被选中了以应用选中的样式
+    hasActiveClass(date) {
+      let fullDate = timeFormat(date.fullDate)
+      let startdate = timeFormat(this.startdate)
+      let enddate = timeFormat(this.enddate)
+      if (this.range) {
+        return fullDate == enddate || fullDate == startdate
+      } else {
+        return fullDate == startdate
+      }
+    },
+
+    // 判断格子是否在被选中的日子之间
+    hasBelongClass(date) {
+      return this.range ? date.fullDate.getTime() > this.startdate.getTime() && date.fullDate.getTime() < this.enddate.getTime() : false
+    },
+
+    // 是否隐藏不属于本月的日子
+    hasHideClass(date) {
+      return date.month != 'current' && this.hiderestdate
+    },
+
+    // 向上翻月
+    getPreMonth() {
+      this.month = this.month - 1 || 12
+      if (this.month == 12) {
+        this.year--
+      }
+    },
+
+    // 向下翻月
+    getNextMonth() {
+      this.month = this.month == 12 ? 1 : this.month + 1
+      if (this.month == 1) {
+        this.year++
+      }
+    }
+  },
+
+  computed: {
+    dates() {
+      return this.getDates(this.year, this.month)
+    }
+  }
+}
+
+</script>
